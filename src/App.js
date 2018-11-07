6a41669424ddb753e6758f6d65a7a358b4da939e
Jowita Stachowiak <crazylittlepolka@gmail.com>
2018-11-07 21:23:50 +0100

    Adding spinning floral wreth in background

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import dandelion from './dandelion_01.jpg';
+import wreth from './wreth.png'
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,9 @@ class App extends Component {
     return (
       <div className="App">
 
+        <img src={wreth} className="back_graphic" alt="floral wreth" /> 
+
+        <h1 className="layout_name">Jowita Stachowiak Front-End Developer</h1>
         <nav className="layout_nav">
           <a href="">About me</a>
           <a href="">Photography</a>
@@ -17,21 +21,22 @@ class App extends Component {
             <i className="fa fa-bars">&#9776;</i>
           </button>
         </nav>
+
         <div className="main">
 
-
-
           <div className="layout_header">
-            <header className="App-header">
-             
+
+            <header className="App-header">             
               <h1 className="App-title">Things are not what they seem nor are they otherwise.</h1>
                <img src={logo} className="App-logo" alt="logo" />
             </header>
           </div>
+
           <div className="layout_photo">
             <img src={dandelion} className="photo" alt="dandelion photo"/>
           </div>
         </div>
+
         <p className="App-intro">
           
         </p>
